b873e1d88da3fe61abeb77066baa9f2731fe1bb5
unknown <Chris@.(none)>
2011-10-03 22:22:15 -0400

    Added my interface. I'm sure it will change drastically

---
+++ ChrisPart/src/umich/pitchcoach/DrawPitches.java
@@ -0,0 +1,13 @@
+package umich.pitchcoach;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawPitches extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
