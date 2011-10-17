4daa72c66cce4853a4b6868868f640ad1458a52f
Noam Samuel <noam@eniac.(none)>
2011-10-17 13:01:08 -0400

    Added noisy overtone pattern generator for use in Pitch Analyzer testing

---
+++ PitchRecognizeAlgorithm/src/umich/pitchcoach/test/OvertonePatternTest.java
@@ -0,0 +1,28 @@
+package umich.pitchcoach.test;
+import java.io.*;
+
+public class OvertonePatternTest {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		NoisyOvertonePattern p = new NoisyOvertonePattern(
+					500,
+					44100,
+					new float[]{1000,1000, 500, 500},
+					100
+				);
+		
+		float[] dat = new float[44100];
+		p.readData(dat);
+		PrintWriter out = new PrintWriter(new FileOutputStream("/home/noam/Workshop/sine/dat1"));
+		for (int i = 0; i < dat.length; i++)
+		{
+			out.println(dat[i]);
+		}
+		out.close();
+	}
+
+}
