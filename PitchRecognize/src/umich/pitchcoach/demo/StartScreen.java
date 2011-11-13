5448424e5dad542f095e93fee6717357aa17d581
Nathan Yu <nathanjyu@gmail.com>
2011-11-13 00:33:32 -0500

    Pretty start screen, removed app's title bar

--- PitchRecognize/src/umich/pitchcoach/demo/StartScreen.java
+++ PitchRecognize/src/umich/pitchcoach/demo/StartScreen.java
@@ -6,17 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.Color;
 
 public class StartScreen extends Activity {
     /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.startscreen);
+    final Button pitchMode = (Button) findViewById(R.id.PitchMButton);
+    pitchMode.setBackgroundColor(Color.TRANSPARENT);
+    pitchMode.setTextColor(Color.argb(255, 109, 207, 246));
     
-    Button pitchMode = (Button) findViewById(R.id.PitchMButton);
     pitchMode.setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
+    	pitchMode.setTextColor(Color.RED);
         Intent myIntent = new Intent(view.getContext(), PitchGraphActivity.class);
         startActivityForResult(myIntent, 0);
       }
